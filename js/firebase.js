54aef99e084172dd6375c71f04eb5ff65af57e54
曾玟凱 <zll12545@gmail.com>
2018-03-10 15:48:52 +0800

    新增編輯leetcode內容頁面

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyChS8SWxzsXLv5lBrum5H85n6OzoTAQVdo",
+    authDomain: "webmio-ffc0b.firebaseapp.com",
+    databaseURL: "https://webmio-ffc0b.firebaseio.com",
+    projectId: "webmio-ffc0b",
+    storageBucket: "webmio-ffc0b.appspot.com",
+    messagingSenderId: "635137472435"
+  };
+firebase.initializeApp(config);
